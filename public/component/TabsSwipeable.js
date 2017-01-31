57d8039955e721c78d6d22382dc033b9ff756afc
Vikas <sakivksg@gmail.com>
2017-02-01 02:14:18 +0530

    Enabled full page swipe.

--- public/component/TabsSwipeable.js
+++ public/component/TabsSwipeable.js
@@ -4,7 +4,7 @@ import SwipeableViews from 'react-swipeable-views';
 
 const styles = {
   headline: { fontSize: 24, paddingTop: 16, marginBottom: 12, fontWeight: 400 },
-  slide: { padding: 10 },
+  slide: { padding: 10, height: '100%' },
 };
 
 export default class TabsExampleSwipeable extends React.Component {
@@ -22,10 +22,7 @@ export default class TabsExampleSwipeable extends React.Component {
           <Tab label="Tab Two" value={1} />
           <Tab label="Tab Three" value={2} />
         </Tabs>
-        <SwipeableViews
-          index={this.state.slideIndex}
-          onChangeIndex={this.handleChange}
-        >
+        <SwipeableViews index={this.state.slideIndex} onChangeIndex={this.handleChange}>
           <div>
             <h2 style={styles.headline}>Tabs with slide effect</h2>
             Swipe to see the next slide.<br />
