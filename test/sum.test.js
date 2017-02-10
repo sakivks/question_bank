132779a5130b9b7f61151cdc1094b3ee0c7f63b5
Vikas <sakivksg@gmail.com>
2017-02-10 17:10:46 +0530

    removed failing wrong cases

--- test/sum.test.js
+++ test/sum.test.js
@@ -4,15 +4,7 @@ test('adds 2 + 3 to equal 5', () => {
   expect(mth.sum(2, 3)).toBe(5);
 });
 
-test('adds 1 + 3 to equal 5', () => {
-  expect(mth.sum(1, 3)).toBe(5);
-});
-
 test('multiply 2 * 3 to equal 6', () => {
   expect(mth.multiply(2, 3)).toBe(6);
-});
-
-test('multiply 3 * 4 to equal 12', () => {
-  expect(mth.multiply(3, 4)).toBe(6);
 });
 
